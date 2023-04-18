dfeb580d27189810511710d77e4fcf2548719ed6
haubul <nurislammalikhov@mail.ru>
2023-04-18 23:22:15 +0500

    Задание 6. Задача 1

---
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/DTO/GitHubRepositoryResponse.java
@@ -0,0 +1,13 @@
+package ru.tinkoff.edu.java.scrapper.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.time.OffsetDateTime;
+
+public record GitHubRepositoryResponse(
+        @JsonProperty("pushed_at")
+        OffsetDateTime pushedAt,
+        @JsonProperty("updated_at")
+        OffsetDateTime updatedAt
+        ) {
+}
